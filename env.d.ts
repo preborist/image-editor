5a1450437cc68609e63479c27298561e04f5010f
preborist <preborist@users.noreply.github.com>
2026-07-03 12:07:48 +0000

    deploy: 3ee99a721a358240be31c01573016875482b6c9b

--- env.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference types="vite/client" />
-
-// ICSS `:export` blocks in SCSS modules resolve to a string-keyed object.
-declare module '*.module.scss' {
-  const classes: Record<string, string>
-  export default classes
-}
